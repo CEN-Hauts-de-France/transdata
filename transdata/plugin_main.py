28abe1a47df13613128c8b526177fca5243a46de
VincentD <vincent.damoy@espaces-naturels.fr>
2021-12-01 15:56:43 +0100

    Ajout de la connexion à la base de données

--- transdata/plugin_main.py
+++ transdata/plugin_main.py
@@ -2,6 +2,7 @@
 
 # PyQGIS
 from qgis.gui import QgisInterface
+from qgis.PyQt import QtWidgets, QtSql
 from qgis.PyQt.QtWidgets import QAction
 
 # Plugin package
@@ -13,13 +14,27 @@ class CenTransdataPlugin:
     def __init__(self, iface: QgisInterface):
         """Constructor.
 
-        :param iface: An interface instance that will be passed to this class which \
-        provides the hook by which you can manipulate the QGIS application at run time.
+        :param iface: An interface instance that will be passed to this \
+        class which provides the hook by which you can manipulate the QGIS \
+        application at run time.
         :type iface: QgsInterface
         """
         self.iface = iface
         self.sql_xtor = SqlExecutor()
         self.trsfgeom_form = FormSettings()
+
+        # Connexion à la base de données
+        self.db = QtSql.QSqlDatabase.addDatabase("QPSQL")
+        # QPSQL = nom du pilote postgreSQL
+        self.db.setHostName("192.168.1.12")
+        self.db.setPort(5432)
+        self.db.setDatabaseName("bdcenpicardie")
+        self.db.setUserName("postgres")
+        self.db.setPassword("burotec")
+        ok = self.db.open()
+        if not ok:
+            QtWidgets.QMessageBox.warning(
+                self, "Alerte", u"La connexion est échouée" + self.db.hostName())
 
     def initGui(self):
         self.action = QAction("Go!", self.iface.mainWindow())
@@ -34,4 +49,4 @@ class CenTransdataPlugin:
         # show the dialog
         self.trsfgeom_form.show()
         # Run the dialog event loop
-        result = self.exec_()
+        #result = self.exec_()
