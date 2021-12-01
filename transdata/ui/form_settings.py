937fb93fcc6daba1c7a8de4147ac309699b93284
VincentD <vincent.damoy@espaces-naturels.fr>
2021-12-01 15:57:27 +0100

    Ajout de la connexion du bouton "recherche" et remplissage de la liste de choix

--- transdata/ui/form_settings.py
+++ transdata/ui/form_settings.py
@@ -34,3 +34,28 @@ class FormSettings(FORM_CLASS, QWidget):
 
         self.setupUi(self)
         self.lbl_title.setText("Paramètres du plugin")
+
+        self.btn_rech.clicked.connect(remplissList)
+
+    def remplissList(self):
+        """Remplissage de la liste"""
+        self.lst_cibles.clear()
+        queryFillList = QtSql.QSqlQuery(self.db)
+        qFillList = u"SELECT codesitep, nomsitep FROM bd_site_cen.site_cen_hdf ORDER BY codesitep"
+        ok = queryFillList.exec_(qFillList)
+        while queryFillList.next():
+            # print (query.value(1).toPyDate().strftime("%Y-%m-%d"))
+            self.ui.cbx_listcible.addItem(
+                str(query.value(0))
+                + " / "
+                + str(query.value(1))
+               ,
+                int(query.value(0)),
+            )
+        # 1er paramètre = ce qu'on affiche,
+        # 2ème paramètre = ce qu'on garde en mémoire pour plus tard
+        if not ok:
+            QtWidgets.QMessageBox.warning(
+                self, "Alerte", u"Requête remplissage liste cibles ratée"
+            )
+        self.ui.cbx_listcible.setCurrentIndex(0)
