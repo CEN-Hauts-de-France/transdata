a6ba8f02dfab7c10a02a8ef1ec7f687e70a36bda
VincentD <vincent.damoy@espaces-naturels.fr>
2021-12-03 09:24:31 +0100

    Connexion à la base

--- transdata/ui/form_settings.py
+++ transdata/ui/form_settings.py
@@ -6,9 +6,10 @@
 
 # standard
 from pathlib import Path
+from PyQt5.QtSql import QSqlDatabase
 
 # PyQGIS
-from qgis.core import QgsProviderRegistry
+from qgis.core import QgsProviderRegistry, QgsAbstractDatabaseProviderConnection
 from qgis.PyQt import QtSql, uic
 from qgis.PyQt.QtWidgets import QWidget
 from qgis.utils import pluginDirectory
@@ -28,11 +29,15 @@ FORM_CLASS, _ = uic.loadUiType(
 # ########## Classes ###############
 # ##################################
 
+class AbstractDatabaseProviderConnection(QgsAbstractDatabaseProviderConnection):
+    def executeSql(self, connection_name):
+        pass
+
 
 class FormSettings(FORM_CLASS, QWidget):
     """Settings form."""
 
-    DB_CONN_NAMES = ("bdcen", "bdcen_admin")
+    DB_CONN_NAMES = ("bdcen", "CEN Picardie User - pgservice")
     DB_TYPES = ("postgres",)
 
     def __init__(self, parent=None):
@@ -83,35 +88,44 @@ class FormSettings(FORM_CLASS, QWidget):
             if connection_name in self.DB_CONN_NAMES:
                 flag_connexion_reperee = True
                 self.cbb_database.setCurrentText(connection_name)
-
+                self.connection_name=str(connection_name)
+                
         if not flag_connexion_reperee:
             self.cbb_database.setEnabled(True)
+
+        self.connection = AbstractDatabaseProviderConnection(connection_name)
 
     def remplissage_liste(self):
         """Remplissage de la liste"""
         with open(Path(self.plg_folder) / "sql/recup_sites.sql", "r") as f:
             sql = f.read()
+        
+        result = self.connection.executeSql(sql)
+        print (self.connection)
+        print (result)
 
         if (
             self.cbx_typCible.currentData(self.cbx_typCible.currentIndex())
             == "Site CEN"
         ):
             self.lst_cibles.clear()
-            queryFillList = QtSql.QSqlQuery(self.db)
-            qFillList = "SELECT codesitep, nomsitep FROM bd_site_cen.site_cen_hdf ORDER BY codesitep"
-            ok = queryFillList.exec_(qFillList)
-            while queryFillList.next():
-                # print (query.value(1).toPyDate().strftime("%Y-%m-%d"))
-                self.lst_cibles.addItem(
-                    str(queryFillList.value(0)) + " / " + str(queryFillList.value(1))
-                )
-            if not ok:
-                self.log(
-                    message="Requête remplissage liste cibles ratée",
-                    log_level=1,
-                    push=True,
-                )
+        
+           # queryFillList = QtSql.QSqlQuery(self.db)
+           # qFillList = "SELECT codesitep, nomsitep FROM bd_site_cen.site_cen_hdf ORDER BY codesitep"
+           # ok = queryFillList.exec_(qFillList)
+           # while queryFillList.next():
+           #     # print (query.value(1).toPyDate().strftime("%Y-%m-%d"))
+           #     self.lst_cibles.addItem(
+           #         str(queryFillList.value(0)) + " / " + str(queryFillList.value(1))
+           #     )
+           # if not ok:
+           #     self.log(
+           #         message="Requête remplissage liste cibles ratée",
+           #         log_level=1,
+           #         push=True,
+           #     )
         else:
             self.lst_cibles.clear()
             self.lst_cibles.addItem("Raté!!! ;-p")
         self.lst_cibles.setCurrentRow(0)
+
