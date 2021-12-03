fa4808e4156061f7c2c8410cca71973f5c71a4b1
VincentD <vincent.damoy@espaces-naturels.fr>
2021-12-03 11:16:52 +0100

    Nettoyage et remplissage liste

--- transdata/ui/form_settings.py
+++ transdata/ui/form_settings.py
@@ -6,7 +6,6 @@
 
 # standard
 from pathlib import Path
-from PyQt5.QtSql import QSqlDatabase
 
 # PyQGIS
 from qgis.core import QgsProviderRegistry, QgsAbstractDatabaseProviderConnection
@@ -29,15 +28,11 @@ FORM_CLASS, _ = uic.loadUiType(
 # ########## Classes ###############
 # ##################################
 
-class AbstractDatabaseProviderConnection(QgsAbstractDatabaseProviderConnection):
-    def executeSql(self, connection_name):
-        pass
-
 
 class FormSettings(FORM_CLASS, QWidget):
     """Settings form."""
 
-    DB_CONN_NAMES = ("bdcen", "CEN Picardie User - pgservice")
+    DB_CONN_NAMES = ("bdcen", "bdcen_admin", "CEN Picardie User - pgservice")
     DB_TYPES = ("postgres",)
 
     def __init__(self, parent=None):
@@ -59,7 +54,13 @@ class FormSettings(FORM_CLASS, QWidget):
         self.show()
 
     def renvoie_base_cible(self):
-        """Retourne la base de données cible"""
+        """Retourne la base de données cible.
+
+        Liste les connexions des types de bases de données définis en attribut de classe.
+        Remplit la liste déroulante.
+        Vérifie que la connexion par défaut est bien présente :
+            - Si oui, bloque la liste déroulante sur cette connexion.
+            - Sinon, laisse le choix à l'utilisateur de sélectionner une connexion."""
         for db_type in self.DB_TYPES:
             # retrouver les connections du type de base de données
             connections = (
@@ -70,7 +71,7 @@ class FormSettings(FORM_CLASS, QWidget):
 
         if not len(connections):
             self.log(
-                message="Aucune connection de type {} trouvée".format(db_type),
+                message="Aucune connexion de type {} trouvée".format(db_type),
                 log_level=1,
                 push=True,
             )  # Run the dialog event loop
@@ -88,44 +89,54 @@ class FormSettings(FORM_CLASS, QWidget):
             if connection_name in self.DB_CONN_NAMES:
                 flag_connexion_reperee = True
                 self.cbb_database.setCurrentText(connection_name)
-                self.connection_name=str(connection_name)
-                
+
         if not flag_connexion_reperee:
             self.cbb_database.setEnabled(True)
 
-        self.connection = AbstractDatabaseProviderConnection(connection_name)
-
-    def remplissage_liste(self):
+    def remplissage_liste_old(self):
         """Remplissage de la liste"""
         with open(Path(self.plg_folder) / "sql/recup_sites.sql", "r") as f:
             sql = f.read()
-        
+
         result = self.connection.executeSql(sql)
-        print (self.connection)
-        print (result)
+        print(self.connection)
+        print(result)
 
         if (
             self.cbx_typCible.currentData(self.cbx_typCible.currentIndex())
             == "Site CEN"
         ):
             self.lst_cibles.clear()
-        
-           # queryFillList = QtSql.QSqlQuery(self.db)
-           # qFillList = "SELECT codesitep, nomsitep FROM bd_site_cen.site_cen_hdf ORDER BY codesitep"
-           # ok = queryFillList.exec_(qFillList)
-           # while queryFillList.next():
-           #     # print (query.value(1).toPyDate().strftime("%Y-%m-%d"))
-           #     self.lst_cibles.addItem(
-           #         str(queryFillList.value(0)) + " / " + str(queryFillList.value(1))
-           #     )
-           # if not ok:
-           #     self.log(
-           #         message="Requête remplissage liste cibles ratée",
-           #         log_level=1,
-           #         push=True,
-           #     )
+
+            # queryFillList = QtSql.QSqlQuery(self.db)
+            # qFillList = "SELECT codesitep, nomsitep FROM bd_site_cen.site_cen_hdf ORDER BY codesitep"
+            # ok = queryFillList.exec_(qFillList)
+            # while queryFillList.next():
+            #     # print (query.value(1).with open(Path(self.plg_folder) / "sql/recup_sites.sql", "r") as f:
+            sql = f.read()
+        # if not ok:
+        #     self.log(
+        #         message="Requête remplissage liste cibles ratée",
+        #         log_level=1,
+        #         push=True,
+        #     )
         else:
             self.lst_cibles.clear()
             self.lst_cibles.addItem("Raté!!! ;-p")
         self.lst_cibles.setCurrentRow(0)
 
+    def remplissage_liste(self):
+        """Récupérer la couche cible choisie par l'utilisateur.
+        On récupère la connexion affichée dans la liste déroulante.
+        On lance la requête qui va récupérer des lignes dans la vue "view_transdata" et la table "secteurs" (dans le schéma bdfauneflore)
+        Remplissage de la liste de choix en fonction du choix de l'utilisateur
+        Filtre sur l'emprise du canevas"""
+
+        table_cible = self.cbx_typCible.currentText()
+        connexion = self.cbb_database.itemData(self.cbb_database.currentIndex())
+        req = "SELECT PostGIS_Version();"
+        result = connexion.executeSql(req)
+        self.log(
+                message=result,
+                log_level=4,
+            )
